19ddcb5c6ccdb7c4be8ba7b17330dd66a4432d56
Swathi <swathi709357@gmail.com>
2021-11-29 15:16:10 +0530

    commit

--- eval.js
+++ eval.js
@@ -10,6 +10,41 @@ const connect = () => {
     return mongooes.connect("mongodb://127.0.0.1:27017/Evaluation")
 };
 
+const userSchema = new mongooes.Schema({
+    company:{type:String, required: true},
+    job:{type:String, required: true},
+    skill:{type:String, required: true},
+    rating:{type:String, required: true},
+    city:{type:String, required: true},
+    notice:{type:String, required: true },
+
+});
+const User = new mongooes.model("user",userSchema);
+
+const postSchema = new mongooes.Schema({
+    company: {type:String, required: true},
+    job:{type:String, required: true},
+    skill:{type:String, required: true},
+    tags :[
+        {type:mongooes.Schema.Types.ObjectId,ref:"tag", required: true}
+    ],
+},
+{
+    versionkey:false,
+    timestamps:true
+});
+
+const Post = mongooes.model("Post",postSchema);
+
+const tagSchema = mongooes.Schema({
+    company:{type:String, required: true },
+    post: {type:mongooes.Schema.Types.ObjectId,ref:"post", required: true}
+
+})
+
+const tag = mongooes.model("tag",tagSchema);
+
+
 
 
 
